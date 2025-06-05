ecfc763109802324b5043a718db605d24533b815
Nate Mann <nathan.mann@me.com>
2025-06-05 13:59:47 -0400

    check if results are expected to be empty
    test example
    
    fixes
    
    fix

--- Tests/StructuredQueriesTests/WhereTests.swift
+++ Tests/StructuredQueriesTests/WhereTests.swift
@@ -40,6 +40,60 @@ extension SnapshotTests {
       }
     }
 
+    @Test func emptyResults() {
+      withKnownIssue("This assert should fail") {
+        assertQuery(
+          Reminder.where(\.isCompleted).and(Reminder.where(\.isFlagged))
+        ) {
+        """
+        SELECT "reminders"."id", "reminders"."assignedUserID", "reminders"."dueDate", "reminders"."isCompleted", "reminders"."isFlagged", "reminders"."notes", "reminders"."priority", "reminders"."remindersListID", "reminders"."title"
+        FROM "reminders"
+        WHERE ("reminders"."isCompleted") AND ("reminders"."isFlagged")
+        """
+        } results: {
+        """
+        ┌────────────────────────────────────────────┐
+        │ Reminder(                                  │
+        │   id: 4,                                   │
+        │   assignedUserID: nil,                     │
+        │   dueDate: Date(2000-06-25T00:00:00.000Z), │
+        │   isCompleted: true,                       │
+        │   isFlagged: false,                        │
+        │   notes: "",                               │
+        │   priority: nil,                           │
+        │   remindersListID: 1,                      │
+        │   title: "Take a walk"                     │
+        │ )                                          │
+        ├────────────────────────────────────────────┤
+        │ Reminder(                                  │
+        │   id: 7,                                   │
+        │   assignedUserID: nil,                     │
+        │   dueDate: Date(2000-12-30T00:00:00.000Z), │
+        │   isCompleted: true,                       │
+        │   isFlagged: false,                        │
+        │   notes: "",                               │
+        │   priority: .low,                          │
+        │   remindersListID: 2,                      │
+        │   title: "Get laundry"                     │
+        │ )                                          │
+        ├────────────────────────────────────────────┤
+        │ Reminder(                                  │
+        │   id: 10,                                  │
+        │   assignedUserID: nil,                     │
+        │   dueDate: Date(2000-12-30T00:00:00.000Z), │
+        │   isCompleted: true,                       │
+        │   isFlagged: false,                        │
+        │   notes: "",                               │
+        │   priority: .medium,                       │
+        │   remindersListID: 3,                      │
+        │   title: "Send weekly emails"              │
+        │ )                                          │
+        └────────────────────────────────────────────┘
+        """
+        }
+      }
+    }
+
     @Test func or() {
       assertQuery(
         Reminder.where(\.isCompleted).or(Reminder.where(\.isFlagged))
