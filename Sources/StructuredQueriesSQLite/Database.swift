40445060e0070ab8390b5fa74a7b28452d5f152a
Stephen Celis <stephen@stephencelis.com>
2025-06-14 13:50:50 -0500

    Begin to decouple query fragments from SQLite
    Currently, query fragments are modeled as strings with `?`s denoting
    bindings, and an array of bindings. Instead we should model things as a
    single array of segments that can be concatenated with whatever form of
    binding identifier we need. For example, Postgres can use `$1`, `$2`,
    etc.
    
    This remodel also has the positive effect of safer debug descriptions of
    our queries.

--- Sources/StructuredQueriesSQLite/Database.swift
+++ Sources/StructuredQueriesSQLite/Database.swift
@@ -127,12 +127,21 @@ public struct Database {
   func withStatement<R>(
     _ query: QueryFragment, body: (OpaquePointer) throws -> R
   ) throws -> R {
+    let (sql, bindings) = query.segments.reduce(into: (sql: "", bindings: [QueryBinding]())) {
+      switch $1 {
+      case .sql(let sql):
+        $0.sql.append(sql)
+      case .binding(let binding):
+        $0.sql.append("?")
+        $0.bindings.append(binding)
+      }
+    }
     var statement: OpaquePointer?
-    let code = sqlite3_prepare_v2(storage.handle, query.string, -1, &statement, nil)
+    let code = sqlite3_prepare_v2(storage.handle, sql, -1, &statement, nil)
     guard code == SQLITE_OK, let statement
     else { throw SQLiteError(db: storage.handle) }
     defer { sqlite3_finalize(statement) }
-    for (index, binding) in zip(Int32(1)..., query.bindings) {
+    for (index, binding) in zip(Int32(1)..., bindings) {
       let result =
         switch binding {
         case .blob(let blob):
