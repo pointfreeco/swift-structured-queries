c8433521319a2d3d3a475cb225afd58a6915ea84
Stephen Celis <stephen@stephencelis.com>
2025-07-22 11:22:46 -0700

    wip

--- Sources/StructuredQueriesCore/SQLite/FTS5.swift
+++ Sources/StructuredQueriesCore/SQLite/FTS5.swift
@@ -1,6 +1,18 @@
+/// A virtual table using the FTS5 extension.
+///
+/// Apply this protocol to a `@Table` declaration to introduce FTS5 helpers.
 public protocol FTS5: Table {}
-
 extension TableDefinition where QueryValue: FTS5 {
+  @available(*, deprecated, message: "Virtual tables are not 'rowid' tables")
+  public var rowid: some QueryExpression<Int> {
+    SQLQueryExpression(
+      """
+      \(QueryValue.self)."rowid"
+      """
+    )
+  }
+
+  /// An expression representing the search result's rank.
   public var rank: some QueryExpression<Double?> {
     SQLQueryExpression(
       """
@@ -9,6 +21,15 @@ extension TableDefinition where QueryValue: FTS5 {
     )
   }
 
+  /// A predicate expression from this table matched against another _via_ the `MATCH` operator.
+  ///
+  /// ```swift
+  /// ReminderText.where { $0.match("get") }
+  /// // SELECT … FROM "reminderTexts" WHERE ("reminderTexts" MATCH 'get')
+  /// ```
+  ///
+  /// - Parameter pattern: A string expression describing the `MATCH` pattern.
+  /// - Returns: A predicate expression.
   public func match(_ pattern: some QueryExpression<String>) -> some QueryExpression<Bool> {
     SQLQueryExpression(
       """
@@ -19,6 +40,12 @@ extension TableDefinition where QueryValue: FTS5 {
 }
 
 extension TableColumnExpression where Root: FTS5 {
+  /// A string expression highlighting matches in this column using the given delimiters.
+  ///
+  /// - Parameters:
+  ///   - open: An opening delimiter denoting the beginning of a match, _e.g._ `"<b>"`.
+  ///   - close: A closing delimiter denoting the end of a match, _e.g._, `"</b>"`.
+  /// - Returns: A string expression highlighting matches in this column.
   public func highlight(
     _ open: String,
     _ close: String
@@ -36,5 +63,18 @@ extension TableColumnExpression where Root: FTS5 {
       )
       """
     )
+  }
+
+  /// A predicate expression from this column matched against another _via_ the `MATCH` operator.
+  ///
+  /// ```swift
+  /// ReminderText.where { $0.title.match("get") }
+  /// // SELECT … FROM "reminderTexts" WHERE ("reminderTexts"."title" MATCH 'get')
+  /// ```
+  ///
+  /// - Parameter pattern: A string expression describing the `MATCH` pattern.
+  /// - Returns: A predicate expression.
+  public func match(_ pattern: some QueryExpression<String>) -> some QueryExpression<Bool> {
+    BinaryOperator(lhs: self, operator: "MATCH", rhs: pattern)
   }
 }
