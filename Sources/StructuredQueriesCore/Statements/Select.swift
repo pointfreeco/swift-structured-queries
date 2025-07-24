416dc7ae469a630d5e56160d32b9e194d68ac8b3
Kay Leung <KayLeung@users.noreply.github.com>
2025-07-24 20:45:22 +0800

    Handle single Join & Where on query helpers

--- Sources/StructuredQueriesCore/Statements/Select.swift
+++ Sources/StructuredQueriesCore/Statements/Select.swift
@@ -1143,6 +1143,37 @@ extension Select {
   /// Creates a new select statement from this one by appending a predicate to its `WHERE` clause.
   ///
   /// - Parameter predicate: A closure that produces a Boolean query expression from this select's
+  ///   tables with a single join.
+  /// - Returns: A new select statement that appends the given predicate to its `WHERE` clause.
+  public func `where`(
+    _ predicate: (From.TableColumns, Joins.TableColumns) -> some QueryExpression<
+      some _OptionalPromotable<Bool?>
+    >
+  ) -> Self
+  where Joins: Table {
+    var select = self
+    select.where.append(predicate(From.columns, Joins.columns).queryFragment)
+    return select
+  }
+
+  /// Creates a new select statement from this one by appending a predicate to its `WHERE` clause.
+  ///
+  /// - Parameter predicate: A result builder closure that returns a Boolean expression to filter
+  ///   by.
+  /// - Returns: A new select statement that appends the given predicate to its `WHERE` clause.
+  public func `where`(
+    @QueryFragmentBuilder<Bool>
+    _ predicate: (From.TableColumns, Joins.TableColumns) -> [QueryFragment]
+  ) -> Self
+  where Joins: Table {
+    var select = self
+    select.where.append(contentsOf: predicate(From.columns, Joins.columns))
+    return select
+  }
+
+  /// Creates a new select statement from this one by appending a predicate to its `WHERE` clause.
+  ///
+  /// - Parameter predicate: A closure that produces a Boolean query expression from this select's
   ///   tables.
   /// - Returns: A new select statement that appends the given predicate to its `WHERE` clause.
   @_disfavoredOverload
