4b9be8f269428abcb926fa584dc15a7d166c2d85
Stephen Celis <stephen@stephencelis.com>
2025-08-29 17:15:06 -0700

    wip

--- Sources/StructuredQueriesSQLite/Macros.swift
+++ Sources/StructuredQueriesSQLite/Macros.swift
@@ -1,5 +1,12 @@
 import StructuredQueriesSQLiteCore
 
+/// Defines and implements a conformance to the ``/StructuredQueriesSQLiteCore/DatabaseFunction``
+/// protocol.
+///
+/// - Parameters
+///   - name: The function's name. Defaults to the name of the function the macro is applied to.
+///   - isDeterministic: Whether or not the function is deterministic (or "pure" or "referentially
+///     transparent"), _i.e._ given an input it will always return the same output.
 @attached(peer, names: overloaded, prefixed(`$`))
 public macro DatabaseFunction(
   _ name: String = "",
--- Sources/StructuredQueriesSQLiteCore/DatabaseFunction.swift
+++ Sources/StructuredQueriesSQLiteCore/DatabaseFunction.swift
@@ -1,6 +1,22 @@
+/// A type representing a database function.
+///
+/// Don't conform to this protocol directly. Instead, use the `@DatabaseFunction` macros to
+/// generate a conformance.
 public protocol DatabaseFunction {
+  /// The name of the function.
   var name: String { get }
+
+  /// The number of arguments the function accepts.
   var argumentCount: Int? { get }
+
+  /// Whether or not the function is deterministic (or "pure" or "referentially transparent"),
+  /// _i.e._ given an input it will always return the same output.
   var isDeterministic: Bool { get }
+
+  /// The function body. Transforms a collection of bindings handed to the function into a binding
+  /// returned to the query.
+  ///
+  /// - Parameter arguments: Arguments passed to the database function.
+  /// - Returns: A value returned from the database function.
   func invoke(_ arguments: [QueryBinding]) -> QueryBinding
 }
