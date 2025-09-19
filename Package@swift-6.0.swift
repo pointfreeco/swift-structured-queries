6e403a19ef83f943b00da0039bd5d2a0da0db82f
Stephen Celis <stephen@stephencelis.com>
2025-09-19 11:11:40 -0700

    Fix warning on Swift 6.0
    We had a merge issue bring in a duplicate target in the Package.swift
    file.

--- Package@swift-6.0.swift
+++ Package@swift-6.0.swift
@@ -21,10 +21,6 @@ let package = Package(
       targets: ["StructuredQueriesCore"]
     ),
     .library(
-      name: "StructuredQueriesTestSupport",
-      targets: ["StructuredQueriesTestSupport"]
-    ),
-    .library(
       name: "StructuredQueriesSQLite",
       targets: ["StructuredQueriesSQLite"]
     ),
