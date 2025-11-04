dd19ae9276aff46b78e77d325d91d2e4e8815e8a
Brandon Williams <mbrandonw@hey.com>
2025-11-04 10:13:23 -0600

    Fix 'map' when calling database functions.

--- Sources/StructuredQueriesCore/Optional.swift
+++ Sources/StructuredQueriesCore/Optional.swift
@@ -275,7 +275,15 @@ extension QueryExpression where QueryValue: _OptionalProtocol {
   public func map<T>(
     _ transform: (SQLQueryExpression<QueryValue.Wrapped>) -> some QueryExpression<T>
   ) -> some QueryExpression<T?> {
-    SQLQueryExpression(transform(SQLQueryExpression(queryFragment)).queryFragment)
+    SQLQueryExpression(
+      """
+      IIF(\(self), \(transform(SQLQueryExpression(queryFragment))), NULL)
+      """
+    )
+    //SQLQueryExpression(transform(SQLQueryExpression(queryFragment)).queryFragment)
+//    Case(self).when(SQLQueryExpression("NULL"), then: SQLQueryExpression("NULL")).else(
+//      SQLQueryExpression(transform(SQLQueryExpression(queryFragment)).queryFragment)
+//    )
   }
 
   /// Creates a new optional expression from this one by applying an unwrapped version of this
