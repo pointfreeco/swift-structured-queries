c900d5963512ee7669f0ed81cae2083ddafce249
Stephen Celis <stephen@stephencelis.com>
2026-01-27 12:21:36 -0800

    wip

--- Package.swift
+++ Package.swift
@@ -178,21 +178,6 @@ let package = Package(
   swiftLanguageModes: [.v6]
 )
 
-if ProcessInfo.processInfo.environment["SPI_GENERATE_DOCS"] != nil
-  || (ProcessInfo.processInfo.environment["GITHUB_ACTION_REPOSITORY"] ?? "").contains(
-    "swift-structured-queries"
-  )  // || true  // NB: Uncomment for local testing in Xcode
-{
-  package.traits.insert(
-    .default(
-      enabledTraits: [
-        "StructuredQueriesCasePaths",
-        "StructuredQueriesTagged",
-      ]
-    )
-  )
-}
-
 let swiftSettings: [SwiftSetting] = [
   .enableUpcomingFeature("MemberImportVisibility")
   // .unsafeFlags([
