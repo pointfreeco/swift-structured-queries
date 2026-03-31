8da8818fccd9959bd683934ddc62cf45bb65b3c8
Go Takagi <15936908+shimastripe@users.noreply.github.com>
2026-04-01 06:34:13 +0900

    Support swift-syntax 605 (#279)

--- Package.swift
+++ Package.swift
@@ -57,7 +57,7 @@ let package = Package(
     .package(url: "https://github.com/pointfreeco/swift-snapshot-testing", from: "1.18.4"),
     .package(url: "https://github.com/pointfreeco/swift-tagged", from: "0.10.0"),
     .package(url: "https://github.com/pointfreeco/xctest-dynamic-overlay", from: "1.5.2"),
-    .package(url: "https://github.com/swiftlang/swift-syntax", "600.0.0"..<"603.0.0"),
+    .package(url: "https://github.com/swiftlang/swift-syntax", "600.0.0"..<"605.0.0"),
   ],
   targets: [
     .target(
