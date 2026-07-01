92cbb9a45b87180889ac669dad6c86c9d4a8ee13
Stephen Celis <stephen@stephencelis.com>
2026-06-30 17:40:38 -0700

    Add `is` operator to `@Selection enum`s
    Allows you to query to filter by a particular case:
    
    ```swift
    Attachment.where { $0.kind.is(\.note) }
    ```

--- Sources/StructuredQueriesCore/Traits/CasePaths.swift
+++ Sources/StructuredQueriesCore/Traits/CasePaths.swift
@@ -1,3 +1,41 @@
-#if !EXCLUDE_EXPORTS && CasePaths
-  @_exported import CasePaths
+#if CasePaths
+  #if EXCLUDE_EXPORTS
+    import CasePaths
+  #else
+    @_exported import CasePaths
+  #endif
+
+  extension ColumnGroup where QueryValue: CasePathable & Table {
+    /// A Boolean query expression that checks if the given enum columns will be decoded for the
+    /// given case.
+    ///
+    /// - Parameter keyPath: A key path from enum columns to a case.
+    /// - Returns: A Boolean query expression
+    public func `is`<V>(
+      _ keyPath: KeyPath<QueryValue.TableColumns, TableColumn<Values.QueryOutput, V>>
+    ) -> some QueryExpression<Bool> {
+      SQLQueryExpression(
+        self[dynamicMember: keyPath]._allColumns.map {
+          "(\($0.queryFragment) IS NOT NULL)"
+        }
+        .joined(separator: " OR ")
+      )
+    }
+
+    /// A Boolean query expression that checks if the given enum columns will be decoded for the
+    /// given case.
+    ///
+    /// - Parameter keyPath: A key path from enum columns to a case.
+    /// - Returns: A Boolean query expression
+    public func `is`<V>(
+      _ keyPath: KeyPath<QueryValue.TableColumns, ColumnGroup<Values.QueryOutput, V>>
+    ) -> some QueryExpression<Bool> {
+      return SQLQueryExpression(
+        self[dynamicMember: keyPath]._allColumns.map {
+          "(\($0.queryFragment) IS NOT NULL)"
+        }
+        .joined(separator: " OR ")
+      )
+    }
+  }
 #endif
