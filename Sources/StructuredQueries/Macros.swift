e844d1c94931e9742e0e9179c8d63fbd2c3a5c92
Stephen Celis <stephen@stephencelis.com>
2026-07-22 16:45:36 -0700

    wip

--- Sources/StructuredQueries/Macros.swift
+++ Sources/StructuredQueries/Macros.swift
@@ -331,22 +331,26 @@ public macro _ColumnCheck<T: QueryBindable & RawRepresentable & Codable>(_ type:
 where T.RawValue: QueryBindable
 
 @attached(peer)
-public macro _ColumnCheck<T: _OptionalProtocol>(_ type: T.Type) =
+public macro _ColumnCheck<T: StructuredQueriesCore._OptionalProtocol>(_ type: T.Type) =
   #externalMacro(module: "StructuredQueriesMacros", type: "ColumnCheckFailRawRepresentableMacro")
 where T.Wrapped: RawRepresentable, T.Wrapped.RawValue: QueryBindable
 
 @attached(peer)
-public macro _ColumnCheck<T: _OptionalProtocol & Codable>(_ type: T.Type) =
+public macro _ColumnCheck<T: StructuredQueriesCore._OptionalProtocol & Codable>(_ type: T.Type) =
   #externalMacro(module: "StructuredQueriesMacros", type: "ColumnCheckFailRawRepresentableMacro")
 where T.Wrapped: RawRepresentable, T.Wrapped.RawValue: QueryBindable
 
 @attached(peer)
-public macro _ColumnCheck<T: _OptionalProtocol & QueryBindable>(_ type: T.Type) =
+public macro _ColumnCheck<T: StructuredQueriesCore._OptionalProtocol & QueryBindable>(
+  _ type: T.Type
+) =
   #externalMacro(module: "StructuredQueriesMacros", type: "ColumnCheckPassMacro")
 where T.Wrapped: RawRepresentable, T.Wrapped.RawValue: QueryBindable
 
 @attached(peer)
-public macro _ColumnCheck<T: _OptionalProtocol & QueryBindable & Codable>(_ type: T.Type) =
+public macro _ColumnCheck<T: StructuredQueriesCore._OptionalProtocol & QueryBindable & Codable>(
+  _ type: T.Type
+) =
   #externalMacro(module: "StructuredQueriesMacros", type: "ColumnCheckPassMacro")
 where T.Wrapped: RawRepresentable, T.Wrapped.RawValue: QueryBindable
 
