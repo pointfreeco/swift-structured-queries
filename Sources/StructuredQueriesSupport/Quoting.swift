5aa6b896ab6ca8e529632307c6bf6d943ed49a40
Stephen Celis <stephen@stephencelis.com>
2026-08-12 19:32:25 -0700

    Query fragment cleanup (#351)
    Noticed a few low hanging improvements to avoid Foundation APIs that
    bridge to NSString, which can be less efficient:
    
      - We bumped our availability so we can use stdlib `replace`
      - Rewrote the `quoted()` helper to be more efficient since it's used
        often in query building

--- Sources/StructuredQueriesSupport/Quoting.swift
+++ Sources/StructuredQueriesSupport/Quoting.swift
@@ -1,13 +1,33 @@
-import Foundation
+public enum QuoteDelimiter {
+  case identifier
+  case text
 
-public enum QuoteDelimiter: String {
-  case identifier = "\""
-  case text = "'"
+  @usableFromInline
+  var ascii: UInt8 {
+    switch self {
+    case .identifier: UInt8(ascii: "\"")
+    case .text: UInt8(ascii: "'")
+    }
+  }
 }
 
 extension StringProtocol {
+  @inlinable
   package func quoted(_ delimiter: QuoteDelimiter = .identifier) -> String {
-    let delimiter = delimiter.rawValue
-    return delimiter + replacingOccurrences(of: delimiter, with: delimiter + delimiter) + delimiter
+    let ascii = delimiter.ascii
+    let scalar = Unicode.Scalar(ascii)
+    var quoted = ""
+    quoted.reserveCapacity(utf8.count + 2)
+    quoted.unicodeScalars.append(scalar)
+    var chunkStart = utf8.startIndex
+    while let index = utf8[chunkStart...].firstIndex(of: ascii) {
+      let after = utf8.index(after: index)
+      quoted.append(contentsOf: self[chunkStart..<after])
+      quoted.unicodeScalars.append(scalar)
+      chunkStart = after
+    }
+    quoted.append(contentsOf: self[chunkStart...])
+    quoted.unicodeScalars.append(scalar)
+    return quoted
   }
 }
