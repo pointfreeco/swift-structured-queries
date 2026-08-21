0635fd4950ec75ff591fa2a8367715f05c153b19
Stephen Celis <stephen@stephencelis.com>
2026-08-21 14:23:16 -0700

    wip

--- Sources/_StructuredQueriesSQLite/DatabaseCollation.swift
+++ Sources/_StructuredQueriesSQLite/DatabaseCollation.swift
@@ -19,13 +19,14 @@ extension DatabaseCollation {
       SQLITE_UTF8,
       body,
       { body, lhsCount, lhs, rhsCount, rhs in
-        let lhs = UnsafeRawBufferPointer(start: lhs, count: Int(lhsCount))
-        let rhs = UnsafeRawBufferPointer(start: rhs, count: Int(rhsCount))
         switch Unmanaged<DatabaseCollationDefinition>
           .fromOpaque(body!)
           .takeUnretainedValue()
           .collation
-          .compare(String(decoding: lhs, as: UTF8.self), String(decoding: rhs, as: UTF8.self))
+          .compare(
+            UnsafeRawBufferPointer(start: lhs, count: Int(lhsCount)),
+            UnsafeRawBufferPointer(start: rhs, count: Int(rhsCount))
+          )
         {
         case .ascending: return -1
         case .same: return 0
